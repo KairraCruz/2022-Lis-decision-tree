46221bbc287f602554b736231efc7b447ba2547e
MrValdez <shunyvaldez@gmail.com>
2022-01-26 18:19:36 +0800

    decision tree completion

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from pprint import pprint
 from sklearn import tree, preprocessing
 from sklearn.preprocessing import MultiLabelBinarizer, LabelEncoder
+from sklearn.model_selection import train_test_split
 
 column_consent = "PRIVACY CONSENT. I understand and agree that by filling out this form, I am allowing the researcher (Kairra Cruz) to collect, process, use, share, and disclose my personal information and also to store it as long as necessary for the fulfillment of Facilities Management Capstone Survey of the stated purpose and in accordance with applicable laws, including the Data Privacy Act of 2012 and its Implementing Rules and Regulations. The purpose and extent of the collection, use, sharing, disclosure, and storage of my personal information were cleared to me. "
 column_multiple_choices = '6. What kind of service/s do you usually request? You can choose more than 1.'
@@ -10,6 +11,7 @@ column_suggestions = "Suggestions to improve existing facilities management proc
 columns_irrelevant_dataset = [column_consent, column_suggestions, "Timestamp", "Email Address"]
 columns_multiple_choice = [column_multiple_choices]
 columns_exclude_one_hot = []
+columns_to_label_encode = None      # if None, all columns are encoded
 
 ranking_order = ["Very Dissatisfied", "Dissatisfied", "Satisfied", "Very Satisfied"]
     
@@ -83,7 +85,7 @@ def get_target_output(records):
         
         print(f"'{column}'")
 
-    choice = input("Choose column to be output (leave empty for default): ").strip()
+    choice = input("Choose column id to be output (leave empty for default): ").strip()
     if choice == "":
         choice = default_target_output
     chosen = records.columns[int(choice) - 1]
@@ -101,7 +103,7 @@ def one_hot_encode_and_bind(original_dataframe, feature_to_encode):
 # Transform multiple choices to list
 # Then one-hot encode records
 # https://stackoverflow.com/a/45312840/1599
-def split_and_one_hot_encode(records, columns_multiple_choice):
+def split_multiple_choice_and_one_hot_encode(records, columns_multiple_choice):
     def split(x):
         # Split the cell, but prevent splitting those that have , in their responses
         # 1. Replace "Top," with "Top|"
@@ -134,22 +136,27 @@ def split_and_one_hot_encode(records, columns_multiple_choice):
 
     return records
 
-def one_hot_encode(records, columns_exclude_one_hot):
+def one_hot_encode(records, columns_to_label_encode):
+    if columns_to_label_encode is None:
+        # if columns_to_label_encode == None, skip encoding columns
+        return records
+
     for column in records.columns:
-        if column == target_output or column in columns_exclude_one_hot:
+        if column == target_output or column in columns_to_label_encode:
             continue
 
         records = one_hot_encode_and_bind(records, column)
 
     return records
 
-def label_encode(records, columns_exclude_one_hot):    
+# if columns_to_label_encode == None, encode all columns
+def label_encode(records, columns_to_label_encode):
     # https://towardsdatascience.com/categorical-encoding-using-label-encoding-and-one-hot-encoder-911ef77fb5bd
-    le = LabelEncoder()
-    le.fit(ranking_order)
+    if columns_to_label_encode is None:
+        columns_to_label_encode = records.columns
 
-    for column in columns_exclude_one_hot:
-        records[column] = le.transform(records[column])
+    for column in columns_to_label_encode:
+        records[column] = LabelEncoder().fit_transform(records[column])
 
     return records
 
@@ -159,17 +166,19 @@ def preprocess_records_for_fitting(records, target_output):
     # 2. Label encode
     # 3. One hot encode responses everything else
 
-    records = split_and_one_hot_encode(records, columns_multiple_choice)
-    records = label_encode(records, columns_exclude_one_hot)
-    records = one_hot_encode(records, columns_exclude_one_hot)
+    records = split_multiple_choice_and_one_hot_encode(records, columns_multiple_choice)
+    records = label_encode(records, columns_to_label_encode)
+    records = one_hot_encode(records, columns_to_label_encode)
 
     return records
 
 def fit_to_model(records, target_output):
-    # generate decision tree    
     X = records.loc[:, records.columns != target_output]
     y = records[target_output]
-    classifier = tree.DecisionTreeClassifier().fit(X, y)
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+    classifier = tree.DecisionTreeClassifier().fit(X_train, y_train)
 
     print(f"Target column is: {target_output}")
     print(f"Outputs are: {y.unique()}")
@@ -177,6 +186,18 @@ def fit_to_model(records, target_output):
     return classifier, X, y
 
 def generate_output(classifier, X, y):
+    def generate_dtreeviz():
+        # https://mljar.com/blog/visualize-decision-tree/
+        from dtreeviz.trees import dtreeviz
+        breakpoint()
+        viz = dtreeviz(classifier, X, y,
+                target_name="target",
+                feature_names=X.columns.tolist(),  
+                #class_names= y.unique(), 
+                class_names= ranking_order, 
+        )
+        viz.save("decision_tree.svg") 
+
     def generate_png():
         # Graphical tree:
         # https://scikit-learn.org/stable/modules/tree.html#classification
@@ -207,7 +228,8 @@ def generate_output(classifier, X, y):
         
         print(f"Output written to {output_textfile}")
 
-    generate_png()
+    generate_dtreeviz()
+    #generate_png()
     generate_txt()
 
 def main():
